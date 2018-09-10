b46ba65b6654590bc3d19ce728498d46531a8629
dk9410 <dk941012@gmail.com>
2018-09-11 09:23:19 +1000

    test purpose only

---
+++ FacebookSettings.cs
@@ -0,0 +1,7 @@
+namespace ConsoleApplication
+{
+    public static class FacebookSettings
+    {
+        public static string AccessToken = "GRAPH_API_ACCESS_TOKEN";
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var facebookClient = new FacebookClient();
+            var facebookService = new FacebookService(facebookClient);
+            var getAccountTask = facebookService.GetAccountAsync(FacebookSettings.AccessToken);
+            Task.WaitAll(getAccountTask);
+            var account = getAccountTask.Result;
+            Console.WriteLine($"{account.Id} {account.Name}");
+
+            var postOnWallTask = facebookService.PostOnWallAsync(FacebookSettings.AccessToken,
+            "Hello from C# .NET Core!");
+            Task.WaitAll(postOnWallTask);
+        }
+    }    
+}
